81d90c59b9dbc610b2ba135e165893bd533c448d
Xijaxa <81866800+Xijaxa@users.noreply.github.com>
2024-02-05 14:08:45 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import MyContainer from './components/MyContainer';
+import MyList from './components/MyList'
+import {useState} from "react"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <MyContainer/>
+    </div>
+  );
+}
+
+export default App;
